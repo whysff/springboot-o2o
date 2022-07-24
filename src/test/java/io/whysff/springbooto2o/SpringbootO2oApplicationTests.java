b75dadea2db1380150f88156cb27fa20deccaf14
whysff <15670151791@126.com>
2022-07-24 13:10:31 +0800

    Initial commit

---
+++ src/test/java/io/whysff/springbooto2o/SpringbootO2oApplicationTests.java
@@ -0,0 +1,13 @@
+package io.whysff.springbooto2o;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootO2oApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
